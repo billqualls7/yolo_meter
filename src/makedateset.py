d89743e13498fe9a807299f62d1ed26cd59aa31a
wuyao <1955416359@qq.com>
2024-03-25 15:46:18 +0800

    ss

--- src/makedateset.py
+++ src/makedateset.py
@@ -2,7 +2,7 @@
 Author: wuyao 1955416359@qq.com
 Date: 2024-03-20 12:31:35
 LastEditors: wuyao 1955416359@qq.com
-LastEditTime: 2024-03-25 15:42:48
+LastEditTime: 2024-03-25 15:45:20
 FilePath: /rqh/dataset/makedateset.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -14,9 +14,9 @@ import os
 from tqdm import tqdm
 
 # 视频文件路径
-video_path = '/home/rqh/dataset/v20.mp4'
+video_path = '/home/rqh/dataset/v30.mp4'
 # 保存帧的文件夹路径
-output_folder = '/home/rqh/dataset/v20'
+output_folder = '/home/rqh/dataset/v30'
 os.makedirs(output_folder, exist_ok=True)
 
 # 打开视频文件
